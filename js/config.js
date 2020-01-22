b454319bf85aa6880e54da7dd9a414e72ed3a87f
subpardaemon <subpardaemon@gmail.com>
2020-01-22 09:52:06 +0000

    BABABABAAA

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-central-1_JpOZUBbuy', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2k3rtge3m1lmreusdti1itcb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
